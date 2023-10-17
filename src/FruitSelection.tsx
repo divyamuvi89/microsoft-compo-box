bf8596f5970a47610e4a452c155121fe17c2290d
divyamuvi89 <divyamuvi89@gmail.com>
2023-10-17 11:09:42 -0700

    Added handleChange

--- src/FruitSelection.tsx
+++ src/FruitSelection.tsx
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 
 interface FruitSelectionComboBoxProps{
 
 }
 interface FruitSelectionState{
-    
+
 }
 const fruits =['Apple', 'Orange', 'Banana', 'Grape','Kiwi','Mango', 'Pear','Pineapple' ];
 const FruitSelection = () => {
@@ -18,15 +18,26 @@ const FruitSelection = () => {
         
     };
 
+    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+        handleSelect(filterFruits(e.target.value)[0]);
+    };
+
     return(
         <div>
             <h2>Choose a fruit:</h2>
             <input 
             type = "text"
             placeholder="Enter your Selection"
+            onChange={handleChange}
             
             />
+            <ul>
+                {filterFruits(selectedFruit || '').map((fruit) => (
+                    <li key={fruit} onClick={()=> handleSelect(fruit)}>{fruit}</li>
+                ))}
+            </ul>
+            {selectedFruit !=='Choose a fruit' && <p>Item Selected: {selectedFruit}</p>}
         </div>
-    )
-}
+    );
+};
 export default FruitSelection;
