b7b28f0f2523a6819248dd22f2272928044ed9f5
divyamuvi89 <divyamuvi89@gmail.com>
2023-10-17 11:30:21 -0700

    Added choose a fruit state

--- src/FruitSelection.tsx
+++ src/FruitSelection.tsx
@@ -7,16 +7,16 @@ interface FruitSelectionState{
 
 }
 const fruits =['Apple', 'Orange', 'Banana', 'Grape','Kiwi','Mango', 'Pear','Pineapple' ];
-const FruitSelection = () => {
-    const [selectedFruit, setselectedFruit] = useState('Choose a fruit');
-    const handleSelect = (fruit:string) =>{
+const FruitSelection:React.FC = () => {
+    const [selectedFruit, setselectedFruit] = useState<string>('Choose a fruit');
+   /* const handleSelect = (fruit:string) =>{
         setselectedFruit(fruit);
     };
     const filterFruits = (input:string):string[] => {
         return fruits.filter((fruit) => 
         fruit.includes(input));
         
-    };
+    };*/
 
     const handleChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
         setselectedFruit(e.target.value);
@@ -26,12 +26,13 @@ const FruitSelection = () => {
         <div>
             <h2>Choose a fruit </h2>
             <select value={selectedFruit} onChange={handleChange}>
+                <option value="Choose a fruit">Choose a Fruit</option>
                 {fruits.map((fruit) => (
                     <option key={fruit} value={fruit}>{fruit}</option>
                 ))}
 
             </select>
-            {selectedFruit ! == 'Choose a fruit' && <p>Item Selected {selectedFruit}</p>}
+           {selectedFruit ! == 'Choose a fruit' && <p>Item Selected {selectedFruit}</p>}
         </div>
     );
 };
